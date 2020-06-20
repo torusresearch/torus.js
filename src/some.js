1325834393c9bea8075de3d4ae6d6ee0116bd1b3
tetratorus <tetratorus@gmail.com>
2020-06-20 12:35:21 +0800

    Log network level errors on failure

--- src/some.js
+++ src/some.js
@@ -2,13 +2,17 @@ export const Some = (promises, predicate) => {
   return new Promise((resolve, reject) => {
     let finishedCount = 0
     const sharedState = { resolved: false }
+    const errorArr = new Array(promises.length).fill(undefined)
     const resultArr = new Array(promises.length).fill(undefined)
+    let predicateError
     promises.forEach((x, index) => {
       x.then((resp) => {
         resultArr[index] = resp
         return undefined
       })
-        .catch((_) => {})
+        .catch((error) => {
+          errorArr[index] = error
+        })
         .finally(() => {
           if (sharedState.resolved) return
           predicate(resultArr.slice(0), sharedState)
@@ -17,11 +21,20 @@ export const Some = (promises, predicate) => {
               resolve(data)
               return undefined
             })
-            .catch((_) => {})
+            .catch((error) => {
+              // log only the last predicate error
+              predicateError = error
+            })
             .finally((_) => {
               finishedCount += 1
               if (finishedCount === promises.length) {
-                reject(new Error(`Unable to resolve enough promises, responses: ${JSON.stringify(resultArr)}`))
+                reject(
+                  new Error(
+                    `Unable to resolve enough promises, errors: ${JSON.stringify(errorArr)}, responses: ${JSON.stringify(resultArr)}, predicate: ${
+                      predicateError.message || predicateError
+                    }`
+                  )
+                )
               }
             })
         })
