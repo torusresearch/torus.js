eb60bd6cd9bf7abcc4818fd877fd25efcc06e505
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-25 18:50:22 +0700

    Improve error messages

--- src/some.js
+++ src/some.js
@@ -1,3 +1,7 @@
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
 export const Some = (promises, predicate) =>
   new Promise((resolve, reject) => {
     let finishedCount = 0
@@ -28,13 +32,23 @@ export const Some = (promises, predicate) =>
             .finally((_) => {
               finishedCount += 1
               if (finishedCount === promises.length) {
-                reject(
-                  new Error(
-                    `Unable to resolve enough promises, errors: ${JSON.stringify(errorArr)}, responses: ${JSON.stringify(resultArr)}, predicate: ${
-                      predicateError?.message || predicateError
-                    }`
+                // Filter same responses with same ID and extract non empty error messages
+                const errors = Object.values(Object.fromEntries(resultArr.map((it) => [it?.id, it.error?.data])))
+                  .filter((it) => typeof it === 'string' && it.length > 0)
+                  .map((it) => (it.startsWith('Error occurred while verifying params') ? capitalizeFirstLetter(it.substr(37)) : it))
+                if (errors.length > 0) {
+                  // Format-able errors
+                  const msg = errors.length > 1 ? `\n${errors.map((it) => `• ${it}`).join('\n')}` : errors[0]
+                  reject(new Error(msg))
+                } else {
+                  reject(
+                    new Error(
+                      `Unable to resolve enough promises, errors: ${JSON.stringify(errorArr)}, responses: ${JSON.stringify(resultArr)}, predicate: ${
+                        predicateError?.message || predicateError
+                      }`
+                    )
                   )
-                )
+                }
               }
             })
         })
