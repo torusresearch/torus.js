9de05a444710a8f66ce3a2858812daba2b51093a
chaitanyapotti <chaitanya.potti@gmail.com>
2021-08-11 16:41:01 +0800

    Fix issue with error being undefined if no response yet

--- src/utils.js
+++ src/utils.js
@@ -46,7 +46,7 @@ export const thresholdSame = (arr, t) => {
   return undefined
 }
 
-export const keyLookup = (endpoints, verifier, verifierId) => {
+export const keyLookup = async (endpoints, verifier, verifierId) => {
   const lookupPromises = endpoints.map((x) =>
     post(
       x,
@@ -56,21 +56,26 @@ export const keyLookup = (endpoints, verifier, verifierId) => {
       })
     ).catch((err) => log.error('lookup request failed', err))
   )
-  return Some(lookupPromises, (lookupResults) => {
-    const lookupShares = lookupResults.filter((x) => x)
-    const errorResult = thresholdSame(
-      lookupShares.map((x) => x && x.error),
-      ~~(endpoints.length / 2) + 1
-    )
-    const keyResult = thresholdSame(
-      lookupShares.map((x) => x && x.result),
-      ~~(endpoints.length / 2) + 1
-    )
-    if (keyResult || errorResult) {
-      return Promise.resolve({ keyResult, errorResult })
-    }
-    return Promise.reject(new Error(`invalid results ${JSON.stringify(lookupResults)}`))
-  }).catch((err) => log.error('Some for keylookup failed', err))
+  try {
+    return Some(lookupPromises, (lookupResults) => {
+      const lookupShares = lookupResults.filter((x1) => x1)
+      const errorResult = thresholdSame(
+        lookupShares.map((x2) => x2 && x2.error),
+        ~~(endpoints.length / 2) + 1
+      )
+      const keyResult = thresholdSame(
+        lookupShares.map((x3) => x3 && x3.result),
+        ~~(endpoints.length / 2) + 1
+      )
+      if (keyResult || errorResult) {
+        return Promise.resolve({ keyResult, errorResult })
+      }
+      return Promise.reject(new Error(`invalid results ${JSON.stringify(lookupResults)}`))
+    })
+  } catch (err) {
+    log.error('Some for keylookup failed', err)
+    return undefined
+  }
 }
 
 export const waitKeyLookup = (endpoints, verifier, verifierId, timeout) =>
@@ -80,7 +85,7 @@ export const waitKeyLookup = (endpoints, verifier, verifierId, timeout) =>
     }, timeout)
   })
 
-export const keyAssign = (endpoints, torusNodePubs, lastPoint, firstPoint, verifier, verifierId) => {
+export const keyAssign = async (endpoints, torusNodePubs, lastPoint, firstPoint, verifier, verifierId) => {
   let nodeNum
   let initialPoint
   if (lastPoint === undefined) {
@@ -96,19 +101,19 @@ export const keyAssign = (endpoints, torusNodePubs, lastPoint, firstPoint, verif
     verifier,
     verifier_id: verifierId.toString(),
   })
-  return post(
-    'https://signer.tor.us/api/sign',
-    data,
-    {
-      headers: {
-        pubKeyX: torusNodePubs[nodeNum].X,
-        pubKeyY: torusNodePubs[nodeNum].Y,
+  try {
+    const signedData = await post(
+      'https://signer.tor.us/api/sign',
+      data,
+      {
+        headers: {
+          pubKeyX: torusNodePubs[nodeNum].X,
+          pubKeyY: torusNodePubs[nodeNum].Y,
+        },
       },
-    },
-    { useAPIKey: true }
-  ).then((signedData) =>
-    // eslint-disable-next-line promise/no-nesting
-    post(
+      { useAPIKey: true }
+    )
+    return post(
       endpoints[nodeNum],
       { ...data, ...signedData },
       {
@@ -116,6 +121,9 @@ export const keyAssign = (endpoints, torusNodePubs, lastPoint, firstPoint, verif
           'Content-Type': 'application/json; charset=utf-8',
         },
       }
-    ).catch((_) => keyAssign(endpoints, torusNodePubs, nodeNum + 1, initialPoint, verifier, verifierId))
-  )
+    )
+  } catch (error) {
+    log.error(error)
+    return keyAssign(endpoints, torusNodePubs, nodeNum + 1, initialPoint, verifier, verifierId)
+  }
 }
