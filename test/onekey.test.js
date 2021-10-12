66433d4f78067501e86a4272d9c928a37ec0d5ae
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-10-12 12:38:43 +0700

    Add login test

--- test/onekey.test.js
+++ test/onekey.test.js
@@ -3,25 +3,42 @@ import { expect } from 'chai'
 import faker from 'faker'
 
 import TorusUtils from '../src/torus'
+import { generateIdToken } from './helpers'
 
 const TORUS_NODE_MANAGER = new NodeManager({ network: 'ropsten', proxyAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183' })
 const TORUS_TEST_EMAIL = 'hello@tor.us'
+const TORUS_TEST_VERIFIER = 'torus-test-health'
 
 describe('torus onekey', function () {
   let torusNodeEndpoints
   let torusNodePub
+  let torusIndexes
   before('one time execution before all tests', async function () {
     const nodeDetails = await TORUS_NODE_MANAGER.getNodeDetails()
     torusNodeEndpoints = nodeDetails.torusNodeEndpoints
     torusNodePub = nodeDetails.torusNodePub
+    torusIndexes = nodeDetails.torusIndexes
   })
 
-  it('should still fetch correct v1 public address', async function () {
+  it('should still fetch v1 public address correctly', async function () {
     const torus = new TorusUtils({ enableOneKey: true, metadataHost: 'https://beta.metadata.tor.us' })
     const verifier = 'google-lrc' // any verifier
     const publicAddress = await torus.getPublicAddress(torusNodeEndpoints, torusNodePub, { verifier, verifierId: TORUS_TEST_EMAIL }, true)
     expect(publicAddress.typeOfUser).to.equal('v1')
     expect(publicAddress.address).to.equal('0xFf5aDad69F4e97AF4D4567e7C333C12df6836a70')
+  })
+
+  it('should still login v1 user correctly', async function () {
+    const torus = new TorusUtils({ enableOneKey: true, metadataHost: 'https://beta.metadata.tor.us' })
+    const token = generateIdToken(TORUS_TEST_EMAIL, 'ES256')
+    const retrieveSharesResponse = await torus.retrieveShares(
+      torusNodeEndpoints,
+      torusIndexes,
+      TORUS_TEST_VERIFIER,
+      { verifier_id: TORUS_TEST_EMAIL },
+      token
+    )
+    expect(retrieveSharesResponse.privKey).to.be.equal('068ee4f97468ef1ae95d18554458d372e31968190ae38e377be59d8b3c9f7a25')
   })
 
   it('should be able to key assign', async function () {
