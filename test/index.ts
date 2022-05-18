ec4e0da8298f35fcc2c030e1285e0ab31e258cde
chaitanyapotti <chaitanya.potti@gmail.com>
2022-05-18 19:02:27 +0800

    Move to typescript

---
+++ test/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+import NodeManager from "@toruslabs/fetch-node-details";
+
+import TorusUtils from "../src/torus";
+
+const fetchNodeDetails = new NodeManager();
+const torus = new TorusUtils({ network: "mainnet" });
+const verifier = "google"; // any verifier
+const verifierId = "hello@tor.us"; // any verifier id
+fetchNodeDetails
+  .getNodeDetails({ verifier, verifierId })
+  .then(({ torusNodeEndpoints, torusNodePub }) => torus.getPublicAddress(torusNodeEndpoints, torusNodePub, { verifier, verifierId }))
+  .then((publicAddress) => console.log(publicAddress))
+  .catch(console.error);
--- test/setup.js
+++ test/setup.js
@@ -1,2 +1,13 @@
-global.fetch = require('node-fetch')
-global.atob = require('atob')
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+require("ts-node").register({ project: path.resolve("tsconfig.json"), transpileOnly: true });
+
+const register = require("@babel/register").default;
+
+register({
+  extensions: [".ts", ".js"],
+  rootMode: "upward",
+});
+
+global.fetch = require("node-fetch");
