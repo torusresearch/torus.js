c21fe99fd9d0f3528308ad7bf1e9554701ab33b6
metalurgical <97008724+metalurgical@users.noreply.github.com>
2022-05-31 10:55:01 +0200

    verification update

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import jwt, { Algorithm } from "jsonwebtoken";
+import { keccak256 } from "web3-utils";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-const jwtPrivateKey = `-----BEGIN PRIVATE KEY-----\n${process.env.JWT_PRIVATE_KEY}\n-----END PRIVATE KEY-----`;
+
+const jwtPrivateKey =   `-----BEGIN PRIVATE KEY-----
+MEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCDfRoQkC62DPWgqUocA
+ZdtVkrhpoFEwTCD3f94hZPc/LA==
+-----END PRIVATE KEY-----`;
+
+export function randomEmail() {
+  return keccak256(Math.random().toString()).slice(2, 20) + "@tor.us";
+}
+
 export const generateIdToken = (email: string, alg: Algorithm) => {
   const iat = Math.floor(Date.now() / 1000);
   const payload = {
--- test/substrate.ts
+++ test/substrate.ts
@@ -5,7 +5,7 @@ import { keccak256 } from "web3-utils";
 import { generatePrivate, getPublicCompressed } from "@toruslabs/eccrypto";
 
 import TorusUtils from "../src/torus";
-// import { generateIdToken } from "./helpers";
+ import { generateIdToken, randomEmail } from "./helpers";
 
 // const TORUS_NODE_MANAGER = new FetchNodeDetails({
 //   network: "ropsten",
@@ -34,16 +34,18 @@ describe("torus YOIUIOUWROIEU", function () {
   });
 
   it("should be able to do full flow", async function () {
-    const verifierContract = "5EXwNCZMSVsjuuC4FqrPC5XHm7hHyq1CBLAqxKBKiBa8Rfmi"; // replacement for verifierName/ identifier
-    const mockToken =
-      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9EYzFSVVV6UkRFeU56WXpPREZHUXpKRk5ERkJSVEEyTXpBMk1qbEZOVU01TVRWRk1EVTRNUSJ9.eyJuaWNrbmFtZSI6ImxlbnRhbjEwMjkiLCJuYW1lIjoibGVudGFuMTAyOUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvNTFhNjU1YTRmYjM0MDkxNzFiNWE2MjM4YzA0ZjAyYjE_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZsZS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMC0wNS0wNFQwMTo0MDowNC4xOTVaIiwiZW1haWwiOiJsZW50YW4xMDI5QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2xlbnRhbi5hdXRoMC5jb20vIiwic3ViIjoiZW1haWx8NWVhNmE1MjA1ODBhMWQ5YWY0MTgzMDQ4IiwiYXVkIjoiTFBEVUdnU3FOUDVtU3hsbEdQMFRFZ0p3UnJOVTBsVkgiLCJpYXQiOjE1ODg1NTY0MDUsImV4cCI6MTU4ODYxNjQwNSwibm9uY2UiOiJvVWY2dzJFZkhvenJEcnBCX3lURnQuZjBDekowY2FqREFGOUV6YXE4Y043In0.TNtgcW7dPF7CG_nW3G3sr8tG0Fu7ZinMy92XuZ6SYuFBEvgA_qhJa9l-df10W5zUcm-LcFcZnOn_5UckRxNV3wNYpcFEX6MVARuTG0eo26TwCvDiTr7wLsqtLJ0ZzuKElHARAVBnmi8_8alTHrbuoV-FR_FzUozEAb5TlnjXHn08-IF578etUCjJU7evT3UTU0ThGTIozNAIGsCuUS1kVuYxtL2n49m2nvIRvNhFzJ5mOPwz1Ruu0KFbdBZQjJ3vH-l7aJXuRW52_RU--QSf2lZ8Giig8s0oys7WmfbcZ-8Z-eTsfbDrWqp2TtqDcxVFxbH46NUaoPNYizZw0rZW2A";
+    const verifierContract = "5G7BfNLCMY3fRnB1sMWHZMk63fn34LjJAxXqmfC1cBKd8jja"; // replacement for verifierName/ identifier
+    //const mockToken =
+    //  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9EYzFSVVV6UkRFeU56WXpPREZHUXpKRk5ERkJSVEEyTXpBMk1qbEZOVU01TVRWRk1EVTRNUSJ9.eyJuaWNrbmFtZSI6ImxlbnRhbjEwMjkiLCJuYW1lIjoibGVudGFuMTAyOUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvNTFhNjU1YTRmYjM0MDkxNzFiNWE2MjM4YzA0ZjAyYjE_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZsZS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMC0wNS0wNFQwMTo0MDowNC4xOTVaIiwiZW1haWwiOiJsZW50YW4xMDI5QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2xlbnRhbi5hdXRoMC5jb20vIiwic3ViIjoiZW1haWx8NWVhNmE1MjA1ODBhMWQ5YWY0MTgzMDQ4IiwiYXVkIjoiTFBEVUdnU3FOUDVtU3hsbEdQMFRFZ0p3UnJOVTBsVkgiLCJpYXQiOjE1ODg1NTY0MDUsImV4cCI6MTU4ODYxNjQwNSwibm9uY2UiOiJvVWY2dzJFZkhvenJEcnBCX3lURnQuZjBDekowY2FqREFGOUV6YXE4Y043In0.TNtgcW7dPF7CG_nW3G3sr8tG0Fu7ZinMy92XuZ6SYuFBEvgA_qhJa9l-df10W5zUcm-LcFcZnOn_5UckRxNV3wNYpcFEX6MVARuTG0eo26TwCvDiTr7wLsqtLJ0ZzuKElHARAVBnmi8_8alTHrbuoV-FR_FzUozEAb5TlnjXHn08-IF578etUCjJU7evT3UTU0ThGTIozNAIGsCuUS1kVuYxtL2n49m2nvIRvNhFzJ5mOPwz1Ruu0KFbdBZQjJ3vH-l7aJXuRW52_RU--QSf2lZ8Giig8s0oys7WmfbcZ-8Z-eTsfbDrWqp2TtqDcxVFxbH46NUaoPNYizZw0rZW2A";
     const endpoints = ["http://[::1]:8800", "http://[::1]:8801", "http://[::1]:8802"];
     // const email = faker.internet.email();
+    let email = randomEmail()
     const verifierDetails = { verifier: verifierContract, verifierId: "email|5ea6a520580a1d9af4183048" };
     // const { torusNodeEndpoints, torusNodePub } = await TORUS_NODE_MANAGER.getNodeDetails(verifierDetails);
     // const { torusNodeEndpoints, torusNodePub } = await TORUS_NODE_MANAGER.getNodeDetails(verifierDetails);
     //  torusNodePub
-    await torus.retrieveShares(endpoints, verifierContract, verifierDetails, mockToken, true);
+    let token = generateIdToken(email, "ES256")
+    await torus.retrieveShares(endpoints, verifierContract, verifierDetails, token, true);
     // expect(publicAddress.typeOfUser).to.equal("v2");
   });
 });
