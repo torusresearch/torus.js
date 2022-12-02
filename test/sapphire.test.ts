7dbeb578983c542cdcc56e61c71138b7f8a2b420
Tai Nguyen <tai.nguyen@tor.us>
2022-12-02 15:47:13 +0700

    add unit tests for hashing verifierID

--- test/sapphire.test.ts
+++ test/sapphire.test.ts
@@ -10,6 +10,7 @@ const TORUS_TEST_EMAIL = "sapphiretest340@tor.us";
 const TORUS_TEST_VERIFIER = "torus-test-health";
 
 const TORUS_TEST_AGGREGATE_VERIFIER = "torus-test-health-aggregate";
+const TORUS_TEST_VERIFIER_ID_HASH = "torus-test-verifierid-hash";
 
 describe.only("torus utils sapphire", function () {
   let torus: TorusUtils;
@@ -56,6 +57,32 @@ describe.only("torus utils sapphire", function () {
     const token = generateIdToken(TORUS_TEST_EMAIL, "ES256");
     const retrieveSharesResponse = await torus.retrieveShares(torusNodeEndpoints, TORUS_TEST_VERIFIER, { verifier_id: TORUS_TEST_EMAIL }, token);
     expect(retrieveSharesResponse.privKey).to.be.equal("ba66d7d6884bf14660236f4be34af4465c8f370c63e10df45d167ee70cd35aac");
+  });
+
+  it("should fetch public address when verifierID hash enabled", async function () {
+    const verifierDetails = { verifier: TORUS_TEST_VERIFIER_ID_HASH, verifierId: TORUS_TEST_EMAIL };
+    const publicAddress = await torus.getPublicAddress(torusNodeEndpoints, verifierDetails);
+    expect(publicAddress).to.equal("0x21954FC6EC83cf6ad7959dFf6a94e44339bFEf76");
+  });
+  it("should fetch user type and public address when verifierID hash enabled", async function () {
+    const verifierDetails = { verifier: TORUS_TEST_VERIFIER_ID_HASH, verifierId: TORUS_TEST_EMAIL };
+    const { address, typeOfUser } = await torus.getUserTypeAndAddress(torusNodeEndpoints, verifierDetails, false);
+    expect(address).to.equal("0x21954FC6EC83cf6ad7959dFf6a94e44339bFEf76");
+    expect(typeOfUser).to.equal("v2");
+  });
+  it("should be able to login when verifierID hash enabled", async function () {
+    const token = generateIdToken(TORUS_TEST_EMAIL, "ES256");
+    const retrieveSharesResponse = await torus.retrieveShares(
+      torusNodeEndpoints,
+      TORUS_TEST_VERIFIER_ID_HASH,
+      { verifier_id: TORUS_TEST_EMAIL },
+      token
+    );
+    expect(retrieveSharesResponse.privKey).to.be.equal("e2e9649e98075429122b4cd3fc34751a466c3baa8b4c6cfae2dc61f1360ca40c");
+
+    const responseToken = retrieveSharesResponse.sessionTokensData[0].token;
+    const tokenObj = JSON.parse(Buffer.from(responseToken, "base64").toString());
+    expect(tokenObj.verifier_id).to.equal(TORUS_TEST_EMAIL);
   });
 
   it.skip("should be able to aggregate login", async function () {
