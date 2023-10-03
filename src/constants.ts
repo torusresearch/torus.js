99bc575df22fe1f54daae041b31780856987f305
weechien <83656073+huggingbot@users.noreply.github.com>
2023-10-03 13:18:12 +0800

    [IRT-741] fix: Remove unused constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 export const JRPC_METHODS = {
   GET_OR_SET_KEY: "GetPubKeyOrKeyAssign",
   COMMITMENT_REQUEST: "CommitmentRequest",
-  IMPORT_SHARE: "ImportShare",
   IMPORT_SHARES: "ImportShares",
   GET_SHARE_OR_KEY_ASSIGN: "GetShareOrKeyAssign",
 };
