d20b635dadc2e7d9e7611dc668195fa61e5210a1
himanshu <himanshuchawla009>
2023-12-12 19:02:17 +0800

    skip ed25519 as us region is not updated yet

--- test/sapphire_devnet_ed25519.test.ts
+++ test/sapphire_devnet_ed25519.test.ts
@@ -21,7 +21,7 @@ const TORUS_TEST_VERIFIER = "torus-test-health";
 const TORUS_TEST_AGGREGATE_VERIFIER = "torus-test-health-aggregate";
 const HashEnabledVerifier = "torus-test-verifierid-hash";
 
-describe.only("torus utils ed25519 sapphire devnet", function () {
+describe.skip("torus utils ed25519 sapphire devnet", function () {
   let torus: TorusUtils;
   let TORUS_NODE_MANAGER: NodeManager;
 
